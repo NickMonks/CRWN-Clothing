7cb9432749af30e1edb8808fb139a916fb727567
Nick Monks <nicolasmg94@gmail.com>
2020-11-28 20:46:46 +0000

    updating stripe integration and updating our favicon icon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HsZvALbYdbIfcIvxHhoSc1El7r3Sxsp9tDI1v8IGdMZteISeq4VZsAc6U5kw8tsC5myXJVcj3CqEp7WahqytmSW00TZTMnxo1'
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label = 'Pay Now'
+            name = 'CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image= ''
+            description={`Your total is £${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+}
+
+export default StripeCheckoutButton;
